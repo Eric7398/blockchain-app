7f4bec0f43e2aa6774329c4704a30befce476596
Eric Wang <72224088+Eric7398@users.noreply.github.com>
2022-01-25 22:34:42 -0800

    Completed Charts

--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import Header from '../components/Header';
 import styled from 'styled-components'
+import Main from '../components/Main';
+import Sidebar from '../components/Sidebar';
 
 const Dashboard = ({ address }) => {
-    return <Header />;
+    return (
+        <Wrapper>
+            <Sidebar />
+            <MainContainer>
+                <Header walletAddress={address} />
+                <Main />
+            </MainContainer>
+        </Wrapper>
+    )
+
 };
 
 export default Dashboard;
@@ -14,6 +25,7 @@ const Wrapper = styled.div`
     width: 100vw;
     background-color: #0a0b0d;
     color: white;
+    overflow: hidden;
 `
 
 const MainContainer = styled.div`
